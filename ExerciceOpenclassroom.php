b9d6efb03b81669bf9333d77988ed009ebadcf1a
Simon Cosandey <simon@cosandey.ch>
2018-04-03 14:04:06 +0200

    ajoute la classe ExerciceOpenclassroom

---
+++ ExerciceOpenclassroom.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Description of ExerciceOpenclassroom
+ *
+ * @author simon
+ */
+class ExerciceOpenclassroom {
+    
+    private $date;
+    
+    private $reussi;
+    
+    public function __construct($date, $reussi = true)
+    {
+        $this->date = new \DateTime();
+        $this->reussi = $reussi;
+    }
+    
+    public function getResult()
+    {
+        if($this->reussi == true){
+            return 'exercice parfaitement réeussi!';
+        }
+        else{
+            return false;
+        }
+    }
+}
